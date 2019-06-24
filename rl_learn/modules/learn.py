1baeac8b2d66388569a96cd249772dc41fa9ceaf
Bobby <bshi1324@gmail.com>
2019-06-24 14:29:13 -0500

    debug

--- rl_learn/modules/learn.py
+++ rl_learn/modules/learn.py
@@ -53,6 +53,7 @@ class LEARN(nn.Module):
             packed_out, (_,_) = self.gru(packed_langs)
             print(packed_out)
             print(packed_out.data.shape)
+            print(packed_out.batch_sizes)
             text_out, _ = pad_packed_sequence(packed_out, batch_first=True)
             text_out = torch.mean(text_out, 1)
 

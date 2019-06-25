672282648635d5fe8a6bc0ae89fc469a9f641d16
Bobby <bshi1324@gmail.com>
2019-06-25 14:47:27 -0500

    commit

--- rl_learn/util/__init__.py
+++ rl_learn/util/__init__.py
@@ -1,4 +1,4 @@
 from rl_learn.util.partition import Partition
 from rl_learn.util.data import Data
 from rl_learn.util.utils import *
-from rl_learn.util.environment import make_env, SucessWrapper
+from rl_learn.util.environment import make_env, SuccessWrapper

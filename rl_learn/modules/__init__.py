d25e2a6337e6dc44d65c26028c40a7c4ebfe3201
Bobby <bshi1324@gmail.com>
2019-06-26 12:15:29 -0500

    commit

--- rl_learn/modules/__init__.py
+++ rl_learn/modules/__init__.py
@@ -1,3 +1,4 @@
+from rl_learn.modules.distributions import Categorical, DiagGaussian
 from rl_learn.modules.core import MLP
 from rl_learn.modules.learn import LEARN
 from rl_learn.modules.policy import Policy

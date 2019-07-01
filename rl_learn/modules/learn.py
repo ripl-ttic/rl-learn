e019af9d4e9906328d823478a7bd31b75bd51afe
Bobby <bshi1324@gmail.com>
2019-07-01 14:11:17 -0500

    commit

--- rl_learn/modules/learn.py
+++ rl_learn/modules/learn.py
@@ -37,17 +37,18 @@ class LEARN(nn.Module):
 
     def forward(self, actions, act_lengths, langs, lang_lengths):
         packed_acts = pack_padded_sequence(actions, act_lengths.cpu().numpy(), batch_first=True, enforce_sorted=False)
-        act_out, _ = self.act_gru(packed_acts)
+        packed_act_out, _ = self.act_gru(packed_acts)
+        act_out, _ = pad_packed_sequence(packed_act_out, batch_first=True)
         act_out = torch.mean(act_out, 1)
 
         langs = langs.long()
         langs = self.emb(langs)
 
-        packed_langs = pack_padded_sequence(langs, langlengths.cpu().numpy(), batch_first=True, enforce_sorted=False)
+        packed_langs = pack_padded_sequence(langs, lang_lengths.cpu().numpy(), batch_first=True, enforce_sorted=False)
 
         packed_langs = packed_langs.float()
-        packed_out, _ = self.gru(packed_langs)
-        text_out, _ = pad_packed_sequence(packed_out, batch_first=True)
+        packed_lang_out, _ = self.gru(packed_langs)
+        text_out, _ = pad_packed_sequence(packed_lang_out, batch_first=True)
         text_out = torch.mean(text_out, 1)
 
         out = torch.cat((text_out, act_out), 1)

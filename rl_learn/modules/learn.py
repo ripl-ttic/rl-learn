0a757a91eb85a3a9ad3e40734f5f9f255c5d3221
Bobby <bshi1324@gmail.com>
2019-07-01 14:50:32 -0500

    commit

--- rl_learn/modules/learn.py
+++ rl_learn/modules/learn.py
@@ -36,7 +36,9 @@ class LEARN(nn.Module):
         nn.init.xavier_uniform_(self.emb.weight)
 
     def forward(self, actions, act_lengths, langs, lang_lengths):
-        print(min(act_lengths))
+        if min(act_lengths) == 0:
+            print(actions)
+            print(act_lengths)
         packed_acts = pack_padded_sequence(actions, act_lengths.cpu().numpy(), batch_first=True, enforce_sorted=False)
         packed_act_out, _ = self.act_gru(packed_acts)
         act_out, _ = pad_packed_sequence(packed_act_out, batch_first=True)
